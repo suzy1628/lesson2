9c4143953784caf9632b7502bb3ccf4498cc36ae
suzy <suzy@users-MacBook-Pro-2.local>
2023-03-29 19:46:14 +0600

    lesson2

---
+++ dart_application_2/bin/dart_application_2.dart
@@ -0,0 +1,24 @@
+void main(){
+  int x = 10;
+  String name = 'Suzy';
+  double y = 1.4;
+  List names = ['Suzy','Aimira', 23, 23, 32,];
+  Map collection = {'name': "Suzy", 'age': 1};
+  final b;
+  b = 13;
+  const d =15;
+  var v = 1.6;
+  dynamic name2 = 'lol';
+  var emojji = '\u0E00';
+  print(x);
+  print(name);
+  print(name2);
+  print(y);
+  print(collection);
+  print(b);
+  print(d);
+  print(v);
+  print(emojji);
+
+}
+
